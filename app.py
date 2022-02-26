c50b2a72fa61c0f85701b5fcd4446cb501a6c0bb
Siddharth Gawande <siddharthgawande555@gmail.com>
2022-02-27 00:48:10 +0530

    removed sidebar

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from predict_page import show_predict_page
 from explore_page import show_explore_page
 
 
-page = st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore"))
+page = st.selectbox("Explore or Predict", ("Predict", "Explore"))
 
 if page == "Predict":
     show_predict_page()
